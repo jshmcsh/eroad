1f868fb7086fbd478b355ec84d1a1e8531a7780d
revolyw <revolyw@yeah.net>
2016-03-13 12:19:27 +0800

    map

--- maven/src/main/webapp/map/map.js
+++ maven/src/main/webapp/map/map.js
@@ -3,8 +3,35 @@ function initBMap() {
     var point = new BMap.Point(116.404, 39.915); // 创建点坐标  
     var point = new BMap.Point(116.404, 39.915); // 创建点坐标  
     map.centerAndZoom(point, 15); //初始化地图，设置中心点坐标和地图级别
-    //初始化地图控件
-    
+    //添加地图类型控件
+    map.addControl(new BMap.MapTypeControl());
+    // 添加带有定位的导航控件
+    var navigationControl = new BMap.NavigationControl({
+        // 靠左上角位置
+        anchor: BMAP_ANCHOR_TOP_LEFT,
+        // LARGE类型
+        type: BMAP_NAVIGATION_CONTROL_LARGE,
+        // 启用显示定位
+        enableGeolocation: true
+    });
+    map.addControl(navigationControl);
+    // 添加定位控件
+    var geolocationControl = new BMap.GeolocationControl();
+    geolocationControl.addEventListener("locationSuccess", function(e) {
+        // 定位成功事件
+        var address = '';
+        address += e.addressComponent.province;
+        address += e.addressComponent.city;
+        address += e.addressComponent.district;
+        address += e.addressComponent.street;
+        address += e.addressComponent.streetNumber;
+        alert("当前定位地址为：" + address);
+    });
+    geolocationControl.addEventListener("locationError", function(e) {
+        // 定位失败事件
+        alert(e.message);
+    });
+    map.addControl(geolocationControl);
     //显示地图实时位置
     map.addEventListener("moveend", function() {
         var currentCenter = map.getCenter();
